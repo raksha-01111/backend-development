9aafd920738f52adbca8cf6c778e271f042009f3
thatsparkiegirl <rakshatembhare4@gmail.com>
2024-08-20 10:40:56 +0530

    utils  code discussion

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(statusCode, message, data = "Success"){
+        this.statusCode = statusCode
+        this.data = data
+        this.message = message
+        this.success = statusCode < 400
+        
+    }
+}
